8f4568c65cb31b317a401f74e076c804b6e7d037
Danail-bit <danilodan693@gmail.com>
2024-10-08 08:46:59 -0300

    Add files via upload

---
+++ Soma.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+int X = 6;
+int Y = 28;
+int SOMA;
+SOMA = X + Y;
+printf("%d", SOMA);
+
+}
---
+++ aula2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main (){
+    int idade;
+printf("informe o tamanho do seu pe:");
+scanf("%d",&idade);
+printf("%d",idade);
+}
